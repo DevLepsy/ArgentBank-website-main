43862e3e7b4fd2cd9e14d3361e9aa93caf992af2
DevLepsy <henryck.paris@outlook.fr>
2024-02-23 16:33:33 +0100

    Refactor React Router imports and lazy load pages

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,35 +1,29 @@
-import React from "react";
+import React, { lazy } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
 import MainLayout from "./layout/MainLayout.jsx";
-import Home from "./pages/Home/Home.jsx";
-import SignIn from "./pages/SignIn/SignIn.jsx";
-import Users from "./pages/User/Users.jsx";
-import Error from "./pages/Error/Error.jsx";
+import Home from "./pages/Home.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import Users from "./pages/Users.jsx";
+import Error from "./pages/Error.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "/signin", element: <SignIn /> },
-      { path: "/user", element: <Users /> },
-      { path: "*", element: <Error /> },
-    ],
-  },
-]);
+const HomeLazy = lazy(() => import("./pages/Home.jsx"));
+const SignInLazy = lazy(() => import("./pages/SignIn.jsx"));
+const UsersLazy = lazy(() => import("./pages/Users.jsx"));
+const ErrorLazy = lazy(() => import("./pages/Error.jsx"));
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </React.StrictMode>
   );
 }
