26aa0db5a267403bf92a5fbbf171c70977e5ff95
DevLepsy <henryck.paris@outlook.fr>
2024-02-23 16:33:43 +0100

    Add Header component

---
+++ FrontEnd/src/layout/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "../components/atoms/Logo/Logo";
+import NavLink from "../components/molecules/NavLink/NavLink";
+import {
+  faCircleUser,
+  faArrowRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { useUserAuth } from "../../hooks/useUserAuth/useUserAuth";
+
+const Header = () => {
+  return <div>NavigationHeader</div>;
+};
+
+export default Header;
