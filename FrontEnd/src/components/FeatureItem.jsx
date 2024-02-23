d435c3af3c12b3ed3c4a9f122f8c51209f3b3446
DevLepsy <henryck.paris@outlook.fr>
2024-02-23 16:33:40 +0100

    Add FeatureItem component

---
+++ FrontEnd/src/components/FeatureItem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const FeatureItem = ({ iconFeature, titleFeature, descriptionFeature }) => {
+  return (
+    <article className="flex flex-1 flex-col items-center p-10">
+      <img
+        src={iconFeature}
+        alt="Chat Icon"
+        className="box-content w-[100px] rounded-full border-[10px] border-secondary p-4"
+      />
+      <h3 className="mb-2 mt-4 text-center text-xl font-bold">
+        {titleFeature}
+      </h3>
+      <p className="mt my-[1em] text-center font-light">{descriptionFeature}</p>
+    </article>
+  );
+};
+
+export default FeatureItem;
